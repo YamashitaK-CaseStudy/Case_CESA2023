efb67f5f3275cbb3c015f41eac6315569839aee1
Hiroto Kuge <mininanoda@gmail.com>
2023-03-10 12:28:58 +0900

    [Update]RotatableObject.csを更新しました まわす小について,自身の軸と外部の軸で行える関数を実装しました。 アニメーションについては今後実装する予定です。

--- Assets/Scripts/StageObjectScript/RotatableObject.cs
+++ Assets/Scripts/StageObjectScript/RotatableObject.cs
@@ -9,21 +9,24 @@ public class RotatableObject : MonoBehaviour{
     [SerializeField] Vector3 _axisCenterLocalPos; // 軸中心座標:ローカル座標軸で指定してください
     [SerializeField] float _axisLength;
 
-    
+    private Vector3 _axisCenterWorldPos;
 
     // Start is called before the first frame update
     void Start(){
+
+        _rotAxis.Normalize();
         
         // オブジェクト固有の軸を可視化
         // LineRendererコンポーネントをゲームオブジェクトにアタッチする
         var lineRenderer = this.GetComponent<LineRenderer>();
 
-
         var selfPos = this.transform.position;  // 自身の座標を取得(中心)
-        var axisCenterWorldPos = selfPos + _axisCenterLocalPos; // 軸座標を計算
+        _axisCenterWorldPos = selfPos + _axisCenterLocalPos; // 軸座標を計算
 
-        var rotAxisStartPos = axisCenterWorldPos + ((_axisLength/2)*_rotAxis);
-        var rotAxisEndPos= axisCenterWorldPos + (-(_axisLength/2)*_rotAxis);
+        var axisHalfLength = _axisLength / 2;
+
+        var rotAxisStartPos = _axisCenterWorldPos + ( axisHalfLength * _rotAxis);
+        var rotAxisEndPos = _axisCenterWorldPos + ( - axisHalfLength * _rotAxis);
 
         var positions = new Vector3[]{
              rotAxisStartPos, // 開始点
@@ -31,13 +34,55 @@ public class RotatableObject : MonoBehaviour{
         };
 
         // 線を引く場所を指定する
-        lineRenderer.SetPositions(positions);
+        lineRenderer.SetPositions(positions); 
+        
+        //RotateSmallAxisSelf();
+    }
+
+    // 自身の軸でまわす小をする
+    void RotateSmallAxisSelf() {
+        var tr = this.transform;
+        
+        // 回転のクォータニオン作成
+        var rotQuat = Quaternion.AngleAxis(180, _rotAxis);
+
+        // 円運動の位置計算
+        var pos = tr.position;
+        pos -= _axisCenterWorldPos;
+        pos = rotQuat * pos;
+        pos += _axisCenterWorldPos;
+
+        tr.position = pos;
+
+        // 向き更新
+        tr.rotation = tr.rotation * rotQuat;
+        
+    }
+
+    // 外部の軸でまわす小をする
+    void RotateSmallAxisExtern(Vector3 rotAxisExturn,Vector3 centerPos) {
+        var tr = this.transform;
+
+        // 回転のクォータニオン作成
+        var rotQuat = Quaternion.AngleAxis(180, rotAxisExturn);
+
+        // 円運動の位置計算
+        var pos = tr.position;
+        pos -= centerPos;
+        pos = rotQuat * pos;
+        pos += centerPos;
+
+        tr.position = pos;
+
+        // 向き更新
+        tr.rotation = tr.rotation * rotQuat;
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+       
     }
+
+
 }
