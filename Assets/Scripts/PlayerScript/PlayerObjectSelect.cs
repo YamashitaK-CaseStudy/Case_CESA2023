89fe800854c041659931845414ee30ac78b27c09
YamaYuki33void <yuuki.y1333@gmail.com>
2023-03-10 12:26:14 +0900

    オブジェクトの識別　Rayを飛ばしてLineLendererで描画

--- Assets/Scripts/PlayerScript/PlayerObjectSelect.cs
+++ Assets/Scripts/PlayerScript/PlayerObjectSelect.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 public partial class Player : MonoBehaviour
 {
     private PlayerInput _input;
+    private LineRenderer _linerenderer;
+    public GameObject _selectGameObject { get; set; } = null;
 
     private Vector3 GetRightStick() {
         var axisx = _input.actions["AngleSelect"].ReadValue<Vector2>().x;
@@ -13,8 +15,14 @@ public partial class Player : MonoBehaviour
         return new Vector3(axisx, axisy, 0);
     }
 
+    [System.Obsolete]
     private void Awake() {
         TryGetComponent(out _input);
+        _linerenderer = gameObject.AddComponent<LineRenderer>();
+
+        _linerenderer.SetWidth(0.1f,0.1f);
+        _linerenderer.SetColors(Color.blue, Color.blue);
+        _linerenderer.material.color = Color.blue;
     }
 
     void StartObjectSelect() {
@@ -24,14 +32,27 @@ public partial class Player : MonoBehaviour
         var pos = this.gameObject.transform.position;
 
         Ray ray = new Ray(pos, GetRightStick() * 100.0f);
-
-
+        _linerenderer.SetPosition(0, pos);
+     
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit)) {
-            Debug.Log(hit.collider.gameObject.name);
+
+            _linerenderer.SetPosition(1, hit.point);
+            _selectGameObject = hit.collider.gameObject;
+        }
+        else {
+            _linerenderer.SetPosition(1, pos + GetRightStick() * 100.0f);
+            _selectGameObject = null;
         }
 
-        Debug.DrawRay(ray.origin, ray.direction * 100.0f,Color.red);
-      //  Debug.Log(GetRightStick());
+        if(_selectGameObject != null) {
+            Debug.Log(_selectGameObject.name);
+        }
+        else {
+            Debug.Log("NULL");
+        }
+
+        //Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.red);
+        //Debug.Log(GetRightStick());
     }
 }
