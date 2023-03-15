99d2129ca9093e71be82c00839aa5d209250aff3
HirotoKuge <mininanoda@gmail.com>
2023-03-15 19:45:46 +0900

    [Update]RotatableObject.csを更新しました ゆっくり回転するようになりました。回転時間は_requiredTimeで変更できます(単位はsec)

--- Assets/Scripts/StageObjectScript/RotatableObject.cs
+++ Assets/Scripts/StageObjectScript/RotatableObject.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 // 回転できるオブジェクトのスクリプト
 public class RotatableObject : MonoBehaviour{
 
-    [SerializeField] private Vector3 _rotAxis;            // 回転軸の向きベクトル
-    [SerializeField] private Vector3 _axisCenterLocalPos; // 軸中心座標:ローカル座標で指定してください
-    [SerializeField] private float _axisLength;           // TEST：軸の長さ
-    [SerializeField] private int _rotPerSec;              // 1回転に必要な時間
+    [SerializeField] private Vector3 _selfRotAxis;              // 自身の回転軸ベクトル
+    [SerializeField] private Vector3 _axisCenterLocalPos;       // 軸中心座標:ローカル座標で指定してください
+    [SerializeField] private float _axisLength;                 // TEST：軸の長さ
+    [SerializeField] private float _rotRequirdTime = 1.0f;      // 1回転に必要な時間(sec)
+    
+    private float _elapsedTime = 0.0f;  // 経過時間
 
+    private Vector3 _axisCenterWorldPos; // 回転軸の中心のワールド座標
 
-    private Vector3 _axisCenterWorldPos;
-
-    private Quaternion _rotQuat;  // 回転のクオータニオン
-    private Quaternion _baceQuat; // 回転はじめのクオータニオン
 
     private bool _isRotating = false;   // 回転してるかフラグ
     private bool _isSpin = false;       // 回転しているかフラグ
@@ -23,7 +22,7 @@ public class RotatableObject : MonoBehaviour{
     void Start(){
 
         // 自身の回転軸の向きを正規化しとく
-        _rotAxis.Normalize();
+        _selfRotAxis.Normalize();
 
         // 軸の中心のワールド座標を計算
         CalkAxisWorldPos();
@@ -42,8 +41,8 @@ public class RotatableObject : MonoBehaviour{
 
         var axisHalfLength = _axisLength / 2;
 
-        var rotAxisStartPos = _axisCenterWorldPos + ( axisHalfLength * _rotAxis);
-        var rotAxisEndPos = _axisCenterWorldPos + ( - axisHalfLength * _rotAxis);
+        var rotAxisStartPos = _axisCenterWorldPos + ( axisHalfLength * _selfRotAxis);
+        var rotAxisEndPos = _axisCenterWorldPos + ( - axisHalfLength * _selfRotAxis);
 
         var positions = new Vector3[]{
              rotAxisStartPos, // 開始点
@@ -56,24 +55,14 @@ public class RotatableObject : MonoBehaviour{
 
     // 自身の軸でまわす小をする
     public void RotateSmallAxisSelf() {
-        var tr = this.transform;
-        
-        // 回転のクォータニオン作成
-        var rotQuat = Quaternion.AngleAxis(180, _rotAxis);
-     
+        if ( _isSpin || _isRotating ) {
+            return;
+        }
+       
+        // フラグを立てる
+        _isRotating = true;
 
-        // 円運動の位置計算
-        var pos = tr.position;
-        pos -= _axisCenterWorldPos;
-        pos = rotQuat * pos;
-        pos += _axisCenterWorldPos;
-
-        tr.position = pos;
-
-        // 向き更新
-        tr.rotation = tr.rotation * rotQuat;
-         
-        
+        _elapsedTime = 0.0f;
     }
 
     // 外部の軸でまわす小をする
@@ -109,33 +98,49 @@ public class RotatableObject : MonoBehaviour{
     // 自身の座標でまわす大をする
     public void SpinAxisSelf() {
         _isSpin = true;
-
-        // 回転のクォータニオン作成
-        _rotQuat = Quaternion.AngleAxis(180, _rotAxis);
     }
 
     // 外部の軸でまわす大をする
-    public void SpinAxisExturn(Vector3 spinCenterPos) {
-    
-    
-        
+    public void SpinAxisExturn(Vector3 spinCenterPos,Vector3 spinAxisVec) {
+         
     }
 
     // Update is called once per frame
     void Update(){
-        if ( _isSpin ) {
-            var tr = this.transform;
+        // 回転中かフラグ
+        if ( _isRotating ) {
+
+            // リクエストデルタタイムを求める
+            // リクエストデルタタイム：デルタタイムを1回転に必要な時間で割った値
+            // これの合算値が1になった時,1回転に必要な時間が経過したことになる
+            float requiredDeltaTime = Time.deltaTime/_rotRequirdTime;
+            _elapsedTime += requiredDeltaTime;
+
+            // 目標回転量*リクエストデルタタイムでそのフレームでの回転角度を求めることができる
+            // リクエストデルタタイムの合算値がちょうど1になるように補正をかけると総回転量は目標回転量と一致する
+            if ( _elapsedTime >= 1 ) {
+                _isRotating = false;
+                requiredDeltaTime -= ( _elapsedTime - 1 ); // 補正
+			}
+
+            // 現在フレームの回転を示す回転のクォータニオン作成
+            var angleAxis = Quaternion.AngleAxis(180 * requiredDeltaTime, _selfRotAxis);
 
             // 円運動の位置計算
+            var tr = transform;
             var pos = tr.position;
+
             pos -= _axisCenterWorldPos;
-            pos = _rotQuat * pos;
+            pos = angleAxis * pos;
             pos += _axisCenterWorldPos;
 
             tr.position = pos;
 
-            // 向き更新
-            tr.rotation = tr.rotation * _rotQuat;
+            tr.rotation = tr.rotation * angleAxis;
+        }
+
+        // 回っているかフラグ
+        if ( _isSpin ) {
         }
     }
 
