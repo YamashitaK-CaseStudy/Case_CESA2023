9f9916b0a83fb0a106025cc9eab870bf1816469c
suzumura tomoki <lufus0721@icloud.com>
2023-03-29 14:34:59 +0900

    [Update]二つのベクトルの間の傾きを計算する関数を作成

--- Assets/Scripts/InputStickRotation.cs
+++ Assets/Scripts/InputStickRotation.cs
@@ -3,34 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-/*＝＝＝＝プレイヤークラスに実装する＝＝＝＝＝BEGIN*/
-//public void OnAngleSelect(InputAction.CallbackContext context)
-//{
-//    /*回転情報の更新*/
-//}
 
-//private static Vector2 oldStickVectorNormalize;
-
-/*＝＝＝＝プレイヤークラスに実装する＝＝＝＝＝END*/
-
-/**
- * スティックの回転状態を取得します
- * @return 1から-1の範囲で値が返ります。
- * 正：右回転　負：左回転
- * ９０度に近いほど絶対値が大きくなります
- * ０の場合はベクトルが同じ向き、または反対を向いています。
- */
-float CalculateStickRotation(Vector2 currentVec, Vector2 oldVec)
+//関数のみだとエラーになるのでクラスに内包する
+public class Calculation
 {
-    currentVec.Normalize();
-    oldVec.Normalize();
+    /**
+     * 二つのベクトルの間における傾きを取得します
+     * @return 1から-1の範囲で値が返ります。
+     * 正：右回転　負：左回転
+     * １、-１　の場合は　９０度です
+     * ０　の場合は　０度または１８０度です
+     */
+    public static float CalculateAngleBetweenVector(Vector2 currentVec, Vector2 oldVec)
+    {
+        currentVec.Normalize();
+        oldVec.Normalize();
 
-    //右に90度回転させる
-    Vector2 work = oldVec;
-    oldVec.x = work.y;
-    oldVec.y = -work.x;
+        //右に90度回転させる
+        Vector2 work = oldVec;
+        oldVec.x = work.y;
+        oldVec.y = -work.x;
 
-    return Vector2.Dot(oldVec, currentVec);
+        return Vector2.Dot(oldVec, currentVec);
+    }
+}
+
+//関数が使われていないエラーを消すための例クラス
+public class Exsample : MonoBehaviour
+{
+
+    public void Start()
+    {
+        angleSelect = GetComponent<PlayerInput>().currentActionMap["AngleSelect"];
+    }
+
+    public void Update()
+    {
+        /*回転情報の更新*/
+        Vector2 currentVec = angleSelect.ReadValue<Vector2>();
+
+        float result = Calculation.CalculateAngleBetweenVector(currentVec, oldStickVector);
+
+        if (result > 0)
+        {
+            //右回転している
+        }
+        else if (result < 0)
+        {
+            //左回転している
+        }
+
+        oldStickVector = currentVec;
+    }
+
+    private InputAction angleSelect;
+    private Vector2 oldStickVector;
 }
 
 
