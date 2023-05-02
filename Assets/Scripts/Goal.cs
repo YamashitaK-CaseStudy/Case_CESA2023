f241c776ca6e917edba727f81c407b2c49783239
HirotoKuge <91269093+HirotoKuge@users.noreply.github.com>
2023-05-03 02:24:33 +0900

    [Update]デバッグ用にリセットを作りました

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    
 	private void OnTriggerEnter(Collider other) {
         if(other.gameObject.transform.root.gameObject.tag != "Player") return;
         SuzumuraTomoki.SceneManager.instance.LoadResult();
     }
+
+
+	public void Update(){
+		if (Input.GetKeyDown(KeyCode.R)){
+			Debug.Log("reset");
+			SuzumuraTomoki.SceneManager.instance.LoadCurrentScene();
+		}
+	}
+
 
 }
