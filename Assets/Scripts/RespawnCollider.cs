c3f654435c21d81d66485bc44d3953a33339c778
YamashitaK-CaseStudy <YamashitaKazuki00005@gmail.com>
2023-05-17 12:37:38 +0900

    [Update]リスポーンのシステム変更 上記に合わせてプレハブの更新

--- Assets/Scripts/RespawnCollider.cs
+++ Assets/Scripts/RespawnCollider.cs
@@ -5,18 +5,12 @@ using UnityEngine;
 public class RespawnCollider : MonoBehaviour
 {
 	// Start is called before the first frame update
-	private Vector3 _respawnPos;
+	[SerializeField] private Vector3 _respawnPos;
 	void Start()
 	{
-		_respawnPos = new Vector3(0,0,0);
 		var mesh = this.transform.GetComponent<MeshRenderer>();
 		Color newColor = new Color(0,0,0,0);
 		mesh.material.color = newColor;
-	}
-
-	public void SetRespawnPosition(Vector3 pos){
-		_respawnPos = pos;
-		Debug.Log(_respawnPos);
 	}
 
 	private void OnTriggerEnter(Collider other) {
@@ -24,16 +18,8 @@ public class RespawnCollider : MonoBehaviour
 		Debug.Log("リスポーン");
 		// 今のリスポーンポイントに飛ばす
 		var Comp = other.gameObject.GetComponent<CharacterController>();
-		// キャラクターコントローラーを一度切る
-		if(Comp != null){
-			Comp.enabled = false;
-		}
 		// 飛ばす
 		other.transform.root.gameObject.transform.position = _respawnPos;
-		// キャラクターコントローラーを起動する
-		if(Comp != null){
-			other.gameObject.GetComponent<CharacterController>().enabled = true;
-		}
 
 		// ダメージ処理
 		other.transform.root.GetComponent<Player>().Damage();
