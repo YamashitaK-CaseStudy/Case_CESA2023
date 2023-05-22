430b5ee69cff509ec652bf436012f9144e64ec34
YamashitaK-CaseStudy <YamashitaKazuki00005@gmail.com>
2023-05-23 00:45:20 +0900

    [Update]バグ修正

--- Assets/Scripts/StageObjectScript/Chain/RotObjChain.cs
+++ Assets/Scripts/StageObjectScript/Chain/RotObjChain.cs
@@ -117,8 +117,19 @@ public partial class RotatableObject : MonoBehaviour
 	}
 
 	private void SetChildHitCheckChainFlg(bool flg){
+		if(_kinds == RotObjkinds.ObjectKind.BoltRotObject)
 		for(int i = 0; i < _childObjChainComp.Length; i++){
+			if(_childObjChainComp[i] == null) continue;
 			_childObjChainComp[i]._isCheckHit = flg;
+		}
+	}
+
+	private void SetChildCheckIntoChain(bool flg){
+		if(_kinds == RotObjkinds.ObjectKind.BoltRotObject)
+		// フラグとコライダーサイズを変更
+		for(int i = 0; i < _childObjHitCheckFloorComp.Length; i++){
+			if(_childObjChainComp[i] == null) continue;
+			_childObjChainComp[i].SetCheckInto(flg);
 		}
 	}
 }
