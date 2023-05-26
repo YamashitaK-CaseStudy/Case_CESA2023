890e3680bf1e80d0e21738ef3cb777c732b58e0e
YamashitaK-CaseStudy <YamashitaKazuki00005@gmail.com>
2023-05-26 19:05:50 +0900

    [tmp ]

---
+++ Assets/Scripts/VCamScript/Stage03_Chain.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Stage03_Chain : MonoBehaviour
+{
+    [SerializeField] Cinemachine.CinemachineVirtualCamera _VCam;
+    private void OnTriggerEnter(Collider other) {
+        if(other.transform.root.tag != "Player") return;
+        _VCam.Priority = 11;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.transform.root.tag != "Player") return;
+        _VCam.Priority = 9;
+    }
+}
