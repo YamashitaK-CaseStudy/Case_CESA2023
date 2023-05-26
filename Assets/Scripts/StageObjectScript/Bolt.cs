1c08a42bd503aa34a6f2732bea7761d02ea5bc70
suzumura tomoki <lufus0721@icloud.com>
2023-05-26 18:01:29 +0900

    [Update]外部からボルトの方向を取得できる機能を追加

--- Assets/Scripts/StageObjectScript/Bolt.cs
+++ Assets/Scripts/StageObjectScript/Bolt.cs
@@ -11,6 +11,16 @@ public class Bolt : RotatableObject
         //定義することで親クラスのFixedUpdateを止める
     }
 
+    /*定数*/
+    public enum UpVectorInWorld
+    {
+        VERTICAL,
+        HORIZONTAL,
+    }
+
+
+    /*変数　非公開*/
+
     [SerializeField] private GameObject _threadObject;//インスペクタで設定
     [SerializeField] private uint _length = 1;
     [SerializeField] private uint _translationLimit = 1;
@@ -68,6 +78,19 @@ public class Bolt : RotatableObject
         set
         {
             _spinningTranslationSpeed = value;
+        }
+    }
+
+    public UpVectorInWorld upVectorInWorld
+    {
+        get
+        {
+            float yVecInW_y = transform.localToWorldMatrix.GetColumn(1).y;
+            if (Math.Abs(yVecInW_y) < 0.01f)
+            {
+                return UpVectorInWorld.HORIZONTAL;
+            }
+            return UpVectorInWorld.VERTICAL;
         }
     }
 
@@ -220,7 +243,7 @@ public class Bolt : RotatableObject
         while (_isRotating)
         {
 
-            
+
 
             UpdateRotate();
             UpdatePositionInRotationWhenChild();
