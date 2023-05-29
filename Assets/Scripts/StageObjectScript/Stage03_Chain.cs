b35b78fc42563d77197c34488382213c732a2280
suzumura tomoki <lufus0721@icloud.com>
2023-05-29 20:55:30 +0900

    [Fix]ファイルを復元

---
+++ Assets/Scripts/StageObjectScript/Stage03_Chain.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Stage03_Chain : MonoBehaviour
+{
+    [SerializeField] Cinemachine.CinemachineVirtualCamera _VCam;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.root.tag != "Player") return;
+        _VCam.Priority = 11;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.tag != "Player") return;
+        _VCam.Priority = 9;
+    }
+}
