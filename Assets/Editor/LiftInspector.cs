e92a5761542804658c2b68946618a40a867428dd
YamaYuki33void <yuuki.y1333@gmail.com>
2023-06-23 16:26:47 +0900

    [Update]リフトの床左右追加、ブロックとの当たり判定

--- Assets/Editor/LiftInspector.cs
+++ Assets/Editor/LiftInspector.cs
@@ -6,19 +6,35 @@ using UnityEditor;
 [CustomEditor(typeof(Lift))]
 public class LiftInspector : Editor {
 
+
+    //  [SerializeField] private GameObject _liftDameobj;
+    //  [SerializeField] private GameObject _liftObjectobj;
+
+   // [SerializeField] private int _RightBlockLength;
+   // [SerializeField] private int _LeftBlockLength;
+
     SerializedProperty _liftDirection;
     SerializedProperty _liftStickobj;
     SerializedProperty _liftRideobj;
+    SerializedProperty _liftDameobj;
+    SerializedProperty _liftObjectobj;
     SerializedProperty _lineLength;
     SerializedProperty _rideBlockIndex;
+    SerializedProperty _RightBlockLength;
+    SerializedProperty _LeftBlockLength;
+
 
     void OnEnable() {
 
         _liftDirection = serializedObject.FindProperty("_liftDirection");
         _liftStickobj = serializedObject.FindProperty("_liftStickobj");
         _liftRideobj = serializedObject.FindProperty("_liftRideobj");
+        _liftDameobj = serializedObject.FindProperty("_liftDameobj");
+        _liftObjectobj = serializedObject.FindProperty("_liftObjectobj");
         _lineLength = serializedObject.FindProperty("_stickLength");
         _rideBlockIndex = serializedObject.FindProperty("_rideBlockIndex");
+        _RightBlockLength = serializedObject.FindProperty("_RightBlockLength");
+        _LeftBlockLength = serializedObject.FindProperty("_LeftBlockLength");
     }
 
     [System.Obsolete]
@@ -29,6 +45,8 @@ public class LiftInspector : Editor {
         EditorGUILayout.PropertyField(_liftDirection, new GUIContent("リフトを動かす向き"));
         EditorGUILayout.PropertyField(_liftStickobj, new GUIContent("リフトを動かす棒のモデル"));
         EditorGUILayout.PropertyField(_liftRideobj, new GUIContent("リフトに乗るモデル"));
+        EditorGUILayout.PropertyField(_liftDameobj, new GUIContent("ダミーオブジェクト"));
+        EditorGUILayout.PropertyField(_liftObjectobj, new GUIContent("オブジェクト"));
         
         if(_liftDirection.enumValueIndex == 0) {
             EditorGUILayout.PropertyField(_lineLength, new GUIContent("上側に追加していく棒の長さ"));
@@ -38,6 +56,8 @@ public class LiftInspector : Editor {
         }
 
         EditorGUILayout.PropertyField(_rideBlockIndex, new GUIContent("リフトの乗る位置(棒の要素数で指定)"));
+        EditorGUILayout.PropertyField(_RightBlockLength, new GUIContent("右側に追加するブロック数"));
+        EditorGUILayout.PropertyField(_LeftBlockLength, new GUIContent("左側に塚するブロック数"));
 
         // 設定の確定
         serializedObject.ApplyModifiedProperties();
